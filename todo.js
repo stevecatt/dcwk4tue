e8509af72729936d4def2470db1df1dfa5ba86e2
Steve Cattanach <stevecatt@att.net>
2019-03-06 09:55:33 -0600

    managed to better move completed tasks

--- todo.js
+++ todo.js
@@ -1,11 +1,12 @@
 let addTask = document.getElementById("addTask")
 let toDoItemBox = document.getElementById("toDoItemBox")
-
+let pending = document.getElementById("pending")
+let doneList = document.getElementById("doneList")
 
 addTask.addEventListener('click',function(){console.log('clicked'),console.log(toDoItemBox.value)
 
 let childDiv = document.createElement('div')
-childDiv.classList="new"
+childDiv.className="new"
 
 let newtask=toDoItemBox.value
 let newCheck= document.createElement("input")
@@ -13,27 +14,43 @@ let newtaskAdd = document.createElement("span")
 let newButton= document.createElement("button")
 newButton.addEventListener('click',function() {console.log('clicked the individual ') })
 newButton.addEventListener('click',function() {this.parentElement.parentElement.removeChild(this.parentElement)})
-newtaskAdd.classList = "lists"
+newtaskAdd.className = "lists"
 newtaskAdd.innerHTML=newtask
 newButton.innerHTML= "Remove"
+newButton.className= "remove-button"
 newCheck.type = "checkbox"
 newCheck.addEventListener('change',function(){console.log(this.parentElement)})
-
+newCheck.addEventListener('change',function() {
 console.log(newCheck.checked)
+let check = newCheck.checked
+if (check == true){{ console.log ("checked")}
+let clone = newtaskAdd.parentElement
+      console.log(clone)
+      
+      doneList.appendChild(clone)}
 
+ else if (check == false){{ console.log ("un checked")
+ let clone = newtaskAdd.parentElement
+       console.log(clone)
+       
+       pending.appendChild(clone)}}})
+   
+
+document.body.appendChild(pending)
 document.body.appendChild(childDiv)
 childDiv.appendChild(newCheck)
 childDiv.appendChild(newtaskAdd)
 childDiv.appendChild(newButton)
-newCheck.addEventListener('change',function(){
+pending.appendChild(childDiv)
+//newCheck.addEventListener('change',function(){
    //clone the parent to move it 
-      let clone = newtaskAdd.parentElement
-      console.log(clone)
-      let doneList = document.getElementById("doneList")
-      doneList.appendChild(clone)
+     // let clone = newtaskAdd.parentElement
+      //console.log(clone)
+      //let doneList = document.getElementById("doneList")
+     // doneList.appendChild(clone)
       
       
-    })
+    
  
 
 }
